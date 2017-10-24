9774cfadb45952f1ef9f63aca55856512a776351
RoyChang <roychang@live.com.au>
2017-10-24 21:27:02 +1100

    Add project files.

---
+++ FileStreaming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileStreaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\oldham.txt";
+            if(File.Exists(path))
+            { File.Delete(path); }
+            File.Create(path);
+            
+        }
+    }
+}
